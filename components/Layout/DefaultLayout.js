05bb1cc0b74ca489d9d1df8ccf83cae5991cbb3f
RyanPrabowo <ryan.prabowo@omniretail.co>
2023-01-26 17:38:28 +0700

    create layout, header and logo

---
+++ components/Layout/DefaultLayout.js
@@ -0,0 +1,14 @@
+import Header from "../Header"
+
+export default function DefaultLayout(props) {
+  return (
+    <>
+      <div className="absolute fixed top-0 inset-x-0 z-20">
+        <Header/>
+      </div>
+      <div className="bg-white min-screen">
+
+      </div>
+    </>
+  )
+}
