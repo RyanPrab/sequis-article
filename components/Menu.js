6d03218e99a16cf18e451779f62f9dc102f29c67
RyanPrabowo <ryan.prabowo@omniretail.co>
2023-01-27 18:20:45 +0700

    fix typo and set active menu

--- components/Menu.js
+++ components/Menu.js
@@ -1,14 +1,18 @@
 import Link from "next/link"
 import { withCategory } from '../utils';
 import { useState, useEffect } from 'react';
+import { useRouter } from "next/router";
 
 function Menu(props) {
   const { categories } = props;
   const { data: categoriesData, error: categoriesError } = categories;
+  const router = useRouter();
+  const isFeatured = router.query.featured ? true : false;
+  const articleId = router.query.id;
 
   const [menu, setMenu] = useState([]);
   const initialMenu = [
-    {id: 0, title: "All Artilce"}
+    {id: 0, title: "All Articles"}
   ];
 
   useEffect(() => {
@@ -22,11 +26,19 @@ function Menu(props) {
       {
         menu.map((menu, index) => {
           const isOdd = index % 2 === 0 ? false : true;
+          let isActive = false;
+
+          if (articleId) {
+            isActive = menu.id === parseInt(articleId);
+          } else if (isFeatured) {
+            isActive = menu.id === 0;
+          }
+
           return (
             <Link
               key={index}
               href={menu.id > 0 ? ({pathname: '/', query: {id: menu.id}}) : ({pathname: '/', query: {featured: true}})}
-              className={`w-48 text-sm text-black font-medium hover:text-orange-500 ${isOdd && ('border-b-2 border-black pb-2')}`}
+              className={`w-48 text-sm text-black font-medium hover:text-orange-500 ${isActive && ('text-orange-500')} ${isOdd && ('border-b-2 border-black pb-2')}`}
             >
               {menu.title}
             </Link>
