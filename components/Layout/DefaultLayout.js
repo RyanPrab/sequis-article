0535dae14ab404e2955796694835c4d32c6bd47c
RyanPrabowo <ryan.prabowo@omniretail.co>
2023-01-27 10:38:00 +0700

    fix layout to render children component

--- components/Layout/DefaultLayout.js
+++ components/Layout/DefaultLayout.js
@@ -2,13 +2,13 @@ import Header from "../Header"
 
 export default function DefaultLayout(props) {
   return (
-    <>
-      <div className="absolute fixed top-0 inset-x-0 z-20">
+    <div className="flex flex-col space-y-4">
+      <div className="top-0 inset-x-0 z-20">
         <Header />
       </div>
       <div className="bg-white min-screen">
-
+        {props.children}
       </div>
-    </>
+    </div>
   )
 }
